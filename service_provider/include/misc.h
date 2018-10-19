434350e2991b3c8da01e69ccd072a8927e3d12d0
Can Kockan <cankockan92@gmail.com>
2018-10-19 15:03:16 -0400

    Added the service_provider side of the CMS functionality

---
+++ service_provider/include/misc.h
@@ -0,0 +1,6 @@
+#ifndef __MISC_H
+#define __MISC_H
+
+void swap(int* a, int* b);
+
+#endif
---
+++ service_provider/src/misc.c
@@ -0,0 +1,8 @@
+#include "misc.h"
+
+void swap(int* a, int* b)
+{
+	int t = *a; 
+	*a = *b; 
+	*b = t; 
+}
