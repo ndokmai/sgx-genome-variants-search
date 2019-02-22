c1aa0c98af61ccd373bce58ba6b5c1518db9aa18
Kaiyuan Zhu <31667172+kyzhu@users.noreply.github.com>
2019-02-21 22:58:10 -0500

    Update enclave_cms.h

--- server/Enclave/enclave_cms.h
+++ server/Enclave/enclave_cms.h
@@ -15,22 +15,16 @@ typedef struct cms
 	float epsilon;				// Error Factor
 	int s_thres;			// For sketch querying when the depth is even
 	int16_t** sketch;
-	float** sketchf;
 	
 	uint64_t* seeds;			// For custom hash functions
 } cms;
 
 void cms_init(uint32_t, uint32_t);
-void cms_init_f(uint32_t, uint32_t);
 void cms_free();
 void cms_setsth(int);
 void cms_update_var(uint64_t, int16_t);
-void cms_update_var_f(uint64_t, float);
 void cms_update_var_row(uint64_t, int16_t, size_t);
-void cms_update_var_row_f(uint64_t, float, size_t);
 int16_t cms_query_median_odd(uint64_t item);
 int16_t cms_query_median_even(uint64_t item);
-float cms_query_median_odd_f(uint64_t);
-float cms_query_median_even_f(uint64_t);
 
 #endif
