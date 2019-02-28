99ed7bbccc3d15b9462d3e83064b6fdacfd25c4e
Kaiyuan Zhu <31667172+kyzhu@users.noreply.github.com>
2019-02-28 15:22:07 -0500

    Update enclave_rhht.h
    Added Pearson correlation coefficient computation module

--- server/Enclave/enclave_rhht.h
+++ server/Enclave/enclave_rhht.h
@@ -6,12 +6,37 @@
 #define LOAD_FACTOR_PERCENT	95
 
 extern struct rhht_table* rhht_snp_table;
+extern struct rhht_table_pcc* rhht_snp_table_pcc;
 
 struct elem
 {
 	uint32_t key;
 	uint16_t case_count;
 	uint16_t control_count;
+};
+
+// rank(PCC^2) = rank[(m * dotprod - s_x * s_y)^2 / (m * s_xx - s_x^2)]
+// Require <= 16000 cases + controls
+// Each elem_pcc occupies 4 * k + 14 bytes
+struct elem_pcc
+{
+	uint32_t key;
+
+	// Sum of squared (uncorrected) genotypes
+	uint16_t ssqg;
+
+	// Dot product between uncorrected genotypes and corrected phenotypes
+	// Equal to dot product between corrected genotypes and corrected phenotypes
+	float dotprod;
+
+	// s_x = sg - sc
+	// sc: sum of corrections to genotypes
+	// sg: sum of uncorrected genotypes
+	float sx;
+	
+	// Projection of genotypes to the k-dimensional subspace from PCA
+	// s_xx = ssqg - <pc_projections, pc_projections>
+	float* pc_projections;
 };
 
 struct rhht_table
@@ -22,16 +47,36 @@ struct rhht_table
 	uint32_t resize_threshold;
 };
 
+struct rhht_table_pcc
+{
+	struct elem_pcc* buffer;
+	uint8_t k; // For initializing elem_pcc;
+	uint32_t num_elems;
+	uint32_t capacity;
+	uint32_t resize_threshold;
+};
 
-void allocate_table(uint32_t capacity);
+void allocate_table(uint32_t);
 
-void construct(uint32_t index, uint32_t key, uint16_t case_count, uint16_t control_count);
+void allocate_table_pcc(uint32_t, uint8_t);
 
-void insert(uint32_t key, uint8_t allele_type, uint8_t patient_status);
+void construct(uint32_t, uint32_t, uint16_t, uint16_t);
+
+void construct_pcc(uint32_t);
+
+void insert(uint32_t, uint8_t, uint8_t);
+
+void insert_pcc(uint32_t);
 
 void grow();
 
-int32_t lookup_index(uint32_t key);
+void grow_pcc();
 
-int32_t find(uint32_t key);
+void set_index(uint32_t, uint32_t, float, float, float, float*);
+
+int32_t lookup_index(uint32_t);
+
+int32_t find(uint32_t);
+
+int32_t find_pcc(uint32_t);
 #endif
